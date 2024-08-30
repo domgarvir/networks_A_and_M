af7bf2b45af6af757ccc65f472ab5ca38aa5fd44
Virginia <domgarvir@gmail.com>
2024-08-31 01:16:52 +0200

    changed functions

--- Functions.py
+++ Functions.py
@@ -5,27 +5,6 @@ import numpy as np
 import itertools as it
 
 
-@register_cell_magic
-def bgc(color, cell=None):
-    script = (
-        "var cell = this.closest('.jp-CodeCell');"
-        "var editor = cell.querySelector('.jp-Editor');"
-        "editor.style.background='{}';"
-        "this.parentNode.removeChild(this)"
-    ).format(color)
-
-    display(HTML('<img src onerror="{}">'.format(script)))
-
-def set_background(color):    
-    script = (
-        "var cell = this.closest('.jp-CodeCell');"
-        "var editor = cell.querySelector('.jp-Editor');"
-        "editor.style.background='{}';"
-        "this.parentNode.removeChild(this)"
-    ).format(color)
-    
-    display(HTML('<img src onerror="{}" style="display:none">'.format(script)))
- 
 def draw_colored_multigraph(MDG):
     
     ## Additionally we can also plot it
